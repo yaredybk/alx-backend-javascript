4ad703378eecfb1e157cfecf46c87da934b7cdb8
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-07-23 04:36:06 +0300

    eslint fixes

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,8 @@
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
 
-  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    sanFranciscoNeighborhoods.push(newNeighborhood);
-    return sanFranciscoNeighborhoods;
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
   };
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
 export default function returnHowManyArguments(...args) {
-    args.length;
+  return args.length;
 }
