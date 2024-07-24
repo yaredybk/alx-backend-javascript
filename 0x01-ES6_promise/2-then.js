e29c32ffea2dddfcaafc85c32935d20abcadfa5d
yared b. <yb12ybk@gmail.com>
2024-07-24 23:04:42 +0300

    task-2_fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@
  * @param {Promise} promise
  */
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then((_) => ({ status: 200, body: "Success" }))
     .catch(() => new Error())
     .finally(() => console.log("Got a response from the API"));
