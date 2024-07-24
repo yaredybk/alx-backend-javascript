aba6025eef071b362146d7966b38dcabf6285775
yared b. <yb12ybk@gmail.com>
2024-07-24 21:55:47 +0300

    e

--- utils.js
+++ utils.js
@@ -1,3 +1,8 @@
+
+/**
+ * dummy upload photo function
+ * @returns {Promise{status: 200,body: 'photo-profile-1'}}
+ */
 export function uploadPhoto() {
     return Promise.resolve({
       status: 200,
@@ -6,6 +11,10 @@ export function uploadPhoto() {
   }
   
   
+  /**
+ * dummy create user function
+ * @returns {Promise{firstName: 'Guillaume', lastName: 'Salva'}}
+ */
   export function createUser() {
     return Promise.resolve({
       firstName: 'Guillaume',
