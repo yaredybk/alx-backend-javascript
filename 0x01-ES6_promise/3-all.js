31e7994e097fa1c631de8ae7dd74e53e78775cfb
yared b. <yb12ybk@gmail.com>
2024-07-30 20:50:55 +0300

    task_3-5

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,9 @@
 import { createUser, uploadPhoto } from "../utils.js";
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then(([p, u]) => {
-    console.log(`${p.body} ${u.firstName} ${u.lastName}`);
-  });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([p, u]) => {
+      console.log(`${p.body} ${u.firstName} ${u.lastName}`);
+    })
+    .catch(() => console.log("Signup system offline"));
 }
