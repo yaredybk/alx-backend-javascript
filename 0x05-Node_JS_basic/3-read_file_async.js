de3ddcc69ad190df82a7a78ddd1a690ef3b74d52
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-09-17 23:24:20 +0300

    init / task 0-3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+  return new Promise(() => {
+    try {
+      const data = fs.readFileSync(filePath, 'utf8');
+      const lines = data
+        .split('\n')
+        .map((line) => line.split(','))
+        .filter((line) => line.length > 1);
+      const header = lines.shift();
+      const fieldInd = header.findIndex((el) => el === 'field');
+      const nameInd = header.findIndex((el) => el === 'firstname');
+      const groups = {};
+      lines.forEach((line) => {
+        const field = line[fieldInd];
+        if (groups[field]) {
+          groups[field].count += 1;
+          groups[field].names.push(line[nameInd]);
+        } else if (field) {
+          groups[field] = { count: 1, names: [line[nameInd]] };
+        }
+      });
+      console.warn(lines);
+      console.log(`Number of students: ${lines.length}`);
+      Object.keys(groups).forEach((key) => {
+        const gr = groups[key];
+        console.log(
+          `Number of students in ${key}: ${gr.count}. List: ${gr.names.join(', ')}`,
+        );
+      });
+    } catch (error) {
+      throw new Error('Cannot load the database');
+    }
+  });
+}
+
+module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,8 @@
+# alx
+## 0x05. NodeJS Basics
+Back-end
+JavaScript
+ES6
+NodeJS
+ExpressJS
+ Weight: 1
