fe799cf3af29b82dcca170c6c8259f4b3aaa8d9d
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-09-21 05:46:38 +0300

    fix 2

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -9,7 +9,7 @@ const calculateNumber = (type, a, b) => {
     case 'DIVIDE':
       return bx ? ax / bx : 'Error';
     default:
-      return 'Invalid Type: USE: SUM | SUBTRACT | DIVIDE';
+      return 0;
   }
 };
 
